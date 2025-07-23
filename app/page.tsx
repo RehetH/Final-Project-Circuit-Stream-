06357237adb3963ef045aadb9388069e9ad80640
RehetH <rehethans@outlook.com>
2025-07-23 11:15:02 -0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -562,7 +562,7 @@ export default function HomePage() {
                             {s}
                           </li>
                         ))
-                      }
+                      )}
                     </ul>
                   )}
                 </div>
